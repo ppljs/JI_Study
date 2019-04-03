d908bc3edfa847f1396823f871e2a4aaeb4ce22b
PauloLemgruberJeunon <paulopljsousa@gmail.com>
2019-04-02 21:57:32 -0300

    Completed the fifth exercise of the chapter 2

--- CtCI/chapter2/sum_lists.py
+++ CtCI/chapter2/sum_lists.py
@@ -32,14 +32,71 @@ def sum_lists(node1, node2):
         result = result.next
 
 
+def sum_i_lists(result, node1, node2, counter=-1):
+    if node1 is None:
+        return 0
+    
+    counter += 1
+    if counter != 0:
+        result.next = linkedlist.LinkedNode(None)
+        result = result.next
+    
+    result.value = node1.value + node2.value + sum_i_lists(result, node1.next, node2.next, counter)
+    carry = result.value // 10
+    result.value %= 10
+
+    if counter != 0:
+        return carry
+    else:
+        if carry > 0:
+            head = linkedlist.LinkedNode(carry)
+            head.next = result
+            return head
+        else:
+            return result
+
+
+def to_same_size(node1, node2):
+    counter1 = 0
+    counter2 = 0
+    h_node1 = node1
+    h_node2 = node2
+
+    while node1 is not None:
+        counter1 += 1
+        node1 = node1.next
+    
+    while node2 is not None:
+        counter2 += 1
+        node2 = node2.next
+
+    counter = abs(counter1 - counter2)
+    to_expand = h_node1 if counter1 < counter2 else h_node2
+
+    for i in range(counter):
+        tmp_node = linkedlist.LinkedNode(0)
+        tmp_node.next = to_expand
+        to_expand = tmp_node
+
+    if counter1 < counter2:
+        return to_expand, h_node2
+    else:
+        return h_node1, to_expand
+
+
+def sum_inverted_lists(node1, node2):
+    node1, node2 = to_same_size(node1, node2)
+    result = linkedlist.LinkedList(None)
+    result.head = sum_i_lists(result.head, node1, node2)
+    return result
+
+
 if __name__ == '__main__':
     ll1 = linkedlist.LinkedList(7)
-    ll1.append([1, 6])
+    ll1.append([1, 6, 0])
     ll2 = linkedlist.LinkedList(5)
     ll2.append([9, 2])
     print(ll1)
     print(ll2)
-    llr = linkedlist.LinkedList(None)
-    llr.head = sum_lists(ll1.head, ll2.head)
-    llr.node = llr.head
-    print(llr)
+    result = sum_inverted_lists(ll1.head, ll2.head)
+    print(result)
