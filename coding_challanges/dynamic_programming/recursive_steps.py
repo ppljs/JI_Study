29772f33433563256859e3bfe9ec1d9c762162c2
Paulo Pinheiro Lemgruber Jeunon Sousa <paulopljsousa@gmail.com>
2020-10-06 22:26:05 -0300

    Update recursive_steps.py

--- coding_challanges/dynamic_programming/recursive_steps.py
+++ coding_challanges/dynamic_programming/recursive_steps.py
@@ -1,5 +1,7 @@
 # https://www.youtube.com/watch?v=5o-kdjv7FD0
 
+from functools import reduce
+
 def get_none_array(size):
     return [None] * size
 
@@ -11,12 +13,14 @@ def num_ways_staircase(n, memory, possible_steps, curr=0):
     if memory[curr]:
         return memory[curr]
 
-    counter = 0
-    for step in possible_steps:
-        counter += num_ways_staircase(n, memory, possible_steps, curr + step)
+    possibilities = reduce(
+        lambda prev, step: prev + num_ways_staircase(n, memory, possible_steps, curr + step),
+        possible_steps,
+        0
+    )
 
-    memory[curr] = counter
-    return counter
+    memory[curr] = possibilities
+    return possibilities
 
 qty = 20
-print(num_ways_staircase(qty, get_none_array(qty), {5, 6, 7}))
+print(num_ways_staircase(qty, get_none_array(qty), {1, 2}))
